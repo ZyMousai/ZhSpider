5069b37a10120e884e54bff4738e5b0c4f0e144b
940127252@qq.com <940127252@qq.com>
2021-11-30 09:04:48 +0800

    知乎爬虫

--- core/handle_file.py
+++ core/handle_file.py
@@ -1,5 +1,6 @@
 """操作输入和输出"""
-import pandas
+import pandas as pd
+import os
 
 
 class HandleFile(object):
@@ -8,8 +9,18 @@ class HandleFile(object):
 
     @staticmethod
     def read_file():
-        p = pandas.read_excel("../template/zh.xlsx")
+        p = pd.read_excel("./template/zh.xlsx")
         return [row for index, row in p.itertuples()]
 
-    def writ_file(self):
-        pass
+    def writ_file(self, data, file_name):
+        _cur_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+        account_info_path = os.path.join(_cur_dir, "results/{}.xlsx".format(file_name))
+        if os.path.exists(account_info_path):
+            vaildip_data = pd.read_excel(account_info_path)
+            append_data = vaildip_data.append(data, ignore_index=True)
+            append_data.to_excel(account_info_path, index=False)
+        else:
+            pd_data = pd.DataFrame(data)
+            pd_data.to_excel(account_info_path, index=False)
+
--- core/spider.py
+++ core/spider.py
@@ -1,49 +1,230 @@
 """zh spider 核心爬虫"""
+from concurrent.futures._base import ALL_COMPLETED, wait
+import time
 import requests
-from util.headers import gen_header
+from util.headers import gen_header, ua_list
 from lxml import etree
 import json
+import math
+import random
+from util.BoundedThreadPool import BoundedThreadPoolExecutor
+from core.handle_file import HandleFile
+
+# from concurrent.futures.thread import ThreadPoolExecutor
+zhihu_spider_pool = BoundedThreadPoolExecutor(max_workers=10)
 
 
 class ZhSpider(object):
 
     def __init__(self):
-        pass
+        self.hf = HandleFile()
 
-    def zhihui_user_info_spider(self, url):
-        url = "https://www.zhihu.com/people/chen-shao-neo"
+    def zhihu_user_info_spider(self, url, cookie):
+        # url = "https://www.zhihu.com/people/chen-shao-neo"
         # headers = gen_header()
+        self.article_infos = {"articles": [], "answers": []}
+
+        print("开始抓取当前url:" + url)
         headers = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
-            "cookies": '_xsrf=Q3z0aZyl1V0QtE2Cgh10mewWEGOiZGuZ; _zap=9d18523a-fdaa-47cf-ba87-7ed9e4ededae; d_c0="AJCQRMQFsBOPTi8EM_O35D-gKCa_1Uu9Sc4=|1631010186"; ISSW=1; l_n_c=1; o_act=login; r_cap_id="N2NjY2ZiMTc5Y2E3NDBhZGJjODdhNWZmNGRiNDljOTU=|1637895868|de25f1c3ae4632e326c1a198895af8cf2e5d0974"; cap_id="MWUzNGUxNmFkMWEzNDVhNGE3MzU5MGJmMzkxZGQ1NTE=|1637895868|f3b324cc3650256f691b29d11785fa5484d45121"; l_cap_id="N2I1ODRmZWIzMTg4NGI0YTllN2VhNDE5Y2MwMzBmMWE=|1637895868|a40ab4f643dfb59ad6a7e919efeeacd1d28c1959"; n_c=1; z_c0="2|1:0|10:1637896321|4:z_c0|92:Mi4xVjFSSkF3QUFBQUFBa0pCRXhBV3dFeVlBQUFCZ0FsVk5nSjZOWWdDcDJtWE9vaXo2LWhvdHVQSWpZV0s4dlhEWWxB|4d28da47cf209b373e909c404409f7dcfce255642f09e0ff32ba206ffd5a57a1"; captcha_session_v2="2|1:0|10:1637896870|18:captcha_session_v2|88:ZzA3U2pLNTAzdUtLMS9qTjNtaTFvQkcrNm9DQjIxci9sbTlkZkVJczEzcERSNTRTQi92Sm9GOStuek9RayswZA==|064adc71a81dad37d07147335e06ade67ac615eea65aa31692d3427e6404f2ec"; captcha_ticket_v2="2|1:0|10:1637896884|17:captcha_ticket_v2|704:eyJ2YWxpZGF0ZSI6IkNOMzFfckRvUTF2NkFoUUZHNE15aVJqb1hjenQwUDJlQTdEd1lxcmp3UF84RUhwODZpckJIenVMeUV0c0d5d05wV0tsQUs2Q29ZR1hrZjBDMDdSMWVmR2dTS3VMeDQ3ZnFIX01OR3pnWmVzNjBrbkRybHNDNkg0Vmg4QmxuWXNDVk1GSm03SnBGRDlDb3g0Wk43eXdOSFNSNk5ZMEwwNlhRY08uc3o0WUJyZVV2bzVwX1dqbGpRelhPSlI1VUpacWJneEkuN0Fua2VCYVdncXZRcS5YemYxeUVjRE9xU1QuTmV1VWN4cGFET3dnSWppazg4bnRnd1h6V21wcEZDUWE2RGVMZUNkNERUTEI5VFJqUWhsLmItYkRlOUtMTUpGVEZQdEVxcGtwaXJzRlYtZUNDMWxfd1c4dXQ0VHhKdy1sS2guNjg0aDF6Tnc4ZjJ4ZUg5aV9RZGg3WkV2T21senBSZU1PMnctWTRwSS5NaVVDbW9jcnBBT0hzbTIwbEY4OFouWXRiU0tIWFVMMkIwSUpLNkJWcS1FVThUMTRVMHlvaENPQ0gxdlpta284Umhhb3guQjJqQzVZSHdYZjJiTnBJLXJWbHBhaEt6dXprQjV5cFJzT0JGejFhZ09zSHhVOUFCb2Nxbk5Ic1NYOGQ3MGxVQUYuN24xUWhFc0hLU2FwMyJ9|822582ea9962fd09ccb07c3d0946bc3c7cbbcf8216db13e1737348966752e5eb"; Hm_lvt_98beee57fd2ef70ccdd5ca52b9740c49=1637551899,1637803777,1637894009,1637994569; KLBRSID=57358d62405ef24305120316801fd92a|1637995065|1637994567; Hm_lpvt_98beee57fd2ef70ccdd5ca52b9740c49=1638009464; NOT_UNREGISTER_WAITING=1'
+            "user-agent": random.choice(ua_list),
+            "cookie": cookie
         }
         req = requests.get(url, headers=headers)
         comtent = etree.HTML(req.text)
         user_info = comtent.xpath('//*[@id="js-initialData"]/text()')[0]
         user_info_json = json.loads(user_info)
-        people_info = list(user_info_json["initialState"]["entities"]["users"].values())[0]
-        account_name = people_info["name"]
+        people_info = user_info_json["initialState"]["entities"]["users"][url.split("/")[-1]]
+        url_token = people_info["urlToken"]
+        self.account_name = people_info["name"]
         employment_record = [{"job_name": i["job"]["name"], "company_name": i["company"]["name"]} for i in
                              people_info["employments"]]
         description = people_info['description']
         followersCount = people_info['followerCount']
         # 点赞总数
         like_total_count = people_info['voteupCount']
-        authentication_info = people_info['badgeV2']["mergedBadges"][0]["description"]
+        if people_info['badgeV2']["mergedBadges"]:
+            authentication_info = people_info['badgeV2']["mergedBadges"][0]["description"]
+        else:
+            authentication_info = ""
         articles_count = people_info['articlesCount']
         answer_count = people_info['answerCount']
+        print("抓取{}基础信息成功！".format(self.account_name))
+        user_data = {
+            "账号名称": self.account_name,
+            "职业经历": employment_record,
+            "个人简介": description,
+            "粉丝数": followersCount,
+            "点赞总数": like_total_count,
+            "认证状态": authentication_info,
+            "文章总数": articles_count,
+            "回答总数": answer_count,
+        }
+        self.hf.writ_file(user_data, "account_info")
+        self.write_data(answer_count, url_token, articles_count, cookie)
 
-        print(account_name, employment_record, description, followersCount, like_total_count, authentication_info,
-              articles_count, answer_count)
-        # print(req.text)
+    def write_data(self, answer_count, url_token, articles_count, cookie):
+        answer_pool = []
+        print("开始抓取回答问题数据,总共{}页".format(str(math.ceil(answer_count / 20))))
+        for i in range(math.ceil(answer_count / 20)):
+            answers_url = "https://www.zhihu.com/api/v4/members/{}/answers?include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cattachment%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Cmark_infos%2Ccreated_time%2Cupdated_time%2Creview_info%2Cexcerpt%2Cis_labeled%2Clabel_info%2Crelationship.is_authorized%2Cvoting%2Cis_author%2Cis_thanked%2Cis_nothelp%2Cis_recognized%3Bdata%5B*%5D.vessay_info%3Bdata%5B*%5D.author.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics%3Bdata%5B*%5D.author.vip_info%3Bdata%5B*%5D.question.has_publishing_draft%2Crelationship&offset={}&limit=20&sort_by=created".format(url_token, 20 * i)
+            # answers_url = "https://www.zhihu.com/api/v4/members/{}/answers?include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cattachment%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Cmark_infos%2Ccreated_time%2Cupdated_time%2Creview_info%2Cexcerpt%2Cis_labeled%2Clabel_info%2Crelationship.is_authorized%2Cvoting%2Cis_author%2Cis_thanked%2Cis_nothelp%2Cis_recognized%3Bdata%5B*%5D.vessay_info%3Bdata%5B*%5D.author.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics%3Bdata%5B*%5D.author.vip_info%3Bdata%5B*%5D.question.has_publishing_draft%2Crelationship&offset=1000&limit=All&sort_by=created".format(url_token, 1000 * i)
+            self.zhihu_user_answer_info(answers_url, cookie)
+            # answer_pool.append(zhihu_spider_pool.submit(self.zhihu_user_answer_info, answers_url, cookie))
+        wait(answer_pool, return_when=ALL_COMPLETED)
+        print("回答问题数据抓取成功!")
+        articles_pool = []
+        print("开始抓取文章数据,总共{}页".format(str(math.ceil(articles_count / 20))))
+        for i in range(math.ceil(articles_count / 20)):
+            articles_url = "https://www.zhihu.com/api/v4/members/{}/articles?include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cattachment%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Cmark_infos%2Ccreated_time%2Cupdated_time%2Creview_info%2Cexcerpt%2Cis_labeled%2Clabel_info%2Crelationship.is_authorized%2Cvoting%2Cis_author%2Cis_thanked%2Cis_nothelp%2Cis_recognized%3Bdata%5B*%5D.vessay_info%3Bdata%5B*%5D.author.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics%3Bdata%5B*%5D.author.vip_info%3Bdata%5B*%5D.question.has_publishing_draft%2Crelationship&offset={}&limit=20&sort_by=created".format(
+                url_token, 20 * i)
+            articles_pool.append(zhihu_spider_pool.submit(self.zhihu_user_articles_info, articles_url, cookie))
+        wait(articles_pool, return_when=ALL_COMPLETED)
+        print("文章数据抓取成功!")
+        article_pool = []
+        answers = self.article_infos['answers']
+        self.article_infos['answers'] = []
+        print("开始抓取回答问题文章内容,总共{}个".format(str(len(answers))))
+        for article in answers:
+            article_pool.append(zhihu_spider_pool.submit(self.zhihu_article_info, article, cookie))
+        wait(article_pool, return_when=ALL_COMPLETED)
+        # print("开始获取作者回答排名")
+        # for index, data in enumerate(self.article_infos['answers']):
+        #     self.author_answer_ranking = []
+        #     author_answer_ranking_pool = []
+        #     for i in range(math.ceil(data["article_answerCount"] / 5)):
+        #         articles_url = "https://www.zhihu.com/api/v4/questions/{}/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cattachment%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Cis_labeled%2Cpaid_info%2Cpaid_info_content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_recognized%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cvip_info%2Cbadge%5B%2A%5D.topics%3Bdata%5B%2A%5D.settings.table_of_content.enabled&platform=desktop&sort_by=default".format(
+        #             str(data["article_user_id"]), 5 * i)
+        #         author_answer_ranking_pool.append(
+        #             zhihu_spider_pool.submit(self.zhihu_answer_ranking, articles_url, cookie))
+        #     wait(author_answer_ranking_pool, return_when=ALL_COMPLETED)
+        #     answer_ranking = sorted(self.author_answer_ranking, key=lambda x: x["voteup_count"], reverse=True)
+        #     for i, answer_info in enumerate(answer_ranking):
+        #         if str(answer_info["user_id"]) == str(data['article_url']).split("/")[-1]:
+        #             self.article_infos["answers"][index]["author_answer_ranking"] = i + 1
+        # print("获取作者回答排名成功！！")
+        # 3. 输出
+        article_titles = []
+        article_urls = []
+        voteup_counts = []
+        comment_counts = []
+        author_answer_ranking = []
+        visit_counts = []
+        article_answerCounts = []
+        article_follower_counts = []
+        for data in self.article_infos["answers"]:
+            article_titles.append(data.get("article_title", ""))
+            article_urls.append(data.get("article_url", ""))
+            voteup_counts.append(data.get("voteup_count", 0))
+            comment_counts.append(data.get("comment_count", 0))
+            author_answer_ranking.append(data.get("author_answer_ranking", 0))
+            visit_counts.append(data.get("visit_count", 0))
+            article_answerCounts.append(data.get("article_answerCount", 0))
+            article_follower_counts.append(data.get("article_follower_count", 0))
 
-    def run(self, urls):
-        if len(urls) >= 10:
-            thread_num = 10
-        else:
-            thread_num = len(urls)
+        for data in self.article_infos["articles"]:
+            article_titles.append(data.get("article_title", ""))
+            article_urls.append(data.get("article_url", ""))
+            voteup_counts.append(data.get("voteup_count", 0))
+            comment_counts.append(data.get("comment_count", 0))
+            author_answer_ranking.append(data.get("author_answer_ranking", 0))
+            visit_counts.append(data.get("visit_count", 0))
+            article_answerCounts.append(data.get("article_answerCount", 0))
+            article_follower_counts.append(data.get("article_follower_count", 0))
 
+        writ_data = {
+            "内容标题": article_titles,
+            "内容链接": article_urls,
+            "点赞数": voteup_counts,
+            "评论数": comment_counts,
+            "作者回答排名": author_answer_ranking,
+            "问题浏览量": visit_counts,
+            "问题关注数": article_answerCounts,
+            "问题回答数": article_follower_counts,
+        }
+        self.hf.writ_file(writ_data, self.account_name)
 
-if __name__ == '__main__':
-    zs = ZhSpider()
-    zs.zhihui_user_info_spider("")
+    def zhihu_user_answer_info(self, url, cookie):
+        try:
+            url_port = url.split(".com")[1]
+            headers = gen_header(url_port)
+            headers["cookie"] = cookie
+            req = requests.get(url, headers=headers, timeout=15)
+            answer_info_json_data = req.json()["data"]
+            for json_data in answer_info_json_data:
+                json_dict = {
+                    "article_title": json_data["question"]['title'],
+                    "article_url": "https://www.zhihu.com/question/{}/answer/{}".format(json_data["question"]['id'],
+                                                                                        json_data["id"]),
+                    "article_user_id": json_data["question"]['id'],
+                    "voteup_count": json_data['voteup_count'],
+                    'comment_count': json_data['comment_count']
+                }
+                self.article_infos["answers"].append(json_dict)
+        except Exception as e:
+            print(e)
+
+    def zhihu_user_articles_info(self, url, cookie):
+        try:
+            url_port = url.split(".com")[1]
+            headers = gen_header(url_port)
+            headers["cookie"] = cookie
+            req = requests.get(url, headers=headers, timeout=15)
+            articles_info_json_data = req.json()["data"]
+            for json_data in articles_info_json_data:
+                json_dict = {
+                    "article_title": json_data['title'],
+                    "article_url": json_data['url'],
+                    "voteup_count": json_data['voteup_count'],
+                    'comment_count': json_data['comment_count']
+                }
+                self.article_infos["articles"].append(json_dict)
+        except:
+            pass
+
+    def zhihu_article_info(self, article, cookie):
+        try:
+            headers = {
+                "user-agent": random.choice(ua_list),
+                "cookie": cookie
+            }
+            req = requests.get(article['article_url'], headers=headers, timeout=15)
+            comtent = etree.HTML(req.text)
+            user_info = comtent.xpath('//*[@id="js-initialData"]/text()')[0]
+            user_info_json = json.loads(user_info)
+            article_info = user_info_json["initialState"]["entities"]["questions"][str(article["article_user_id"])]
+            article["visit_count"] = article_info['visitCount']
+            article["article_answerCount"] = article_info['answerCount']
+            article["article_follower_count"] = article_info['followerCount']
+            self.article_infos["answers"].append(article)
+        except:
+            pass
+
+    def zhihu_answer_ranking(self, url, cookie):
+        try:
+            time.sleep(1)
+            url_port = url.split(".com")[1]
+            headers = gen_header(url_port)
+            headers["cookie"] = cookie
+            req = requests.get(url, headers=headers, timeout=15)
+            answer_info_json_data = req.json()["data"]
+            for json_data in answer_info_json_data:
+                json_dict = {
+                    "user_id": json_data['id'],
+                    "voteup_count": json_data['voteup_count'],
+                }
+                self.author_answer_ranking.append(json_dict)
+        except Exception as e:
+            print(e)
+
+    def run(self, urls, cookie):
+        for url in urls:
+            self.zhihu_user_info_spider(url, cookie)
+        # if len(urls) >= 10:
+        #     thread_num = 10
+        # else:
+        #     thread_num = len(urls)
+
+# if __name__ == '__main__':
+#     zs = ZhSpider()
+# _zap=29c72fff-022b-449e-bb02-3b2a617a0ad0; d_c0="AHCfcI0r9BKPTowWLggkfMGi5uRMk3Xf-rw=|1618403625"; _9755xjdesxxd_=32; YD00517437729195%3AWM_TID=U4VYLawMe4tFRQEBERY6thCW5xjMcZFF; _xsrf=bdd0faf3-f29c-48f8-b6d0-8b00d9db95c9; Hm_lvt_98beee57fd2ef70ccdd5ca52b9740c49=1636547336,1637502034,1638016430; gdxidpyhxdE=ThDtiXp%5CRj%2FNt%2BkQldqQP5mQ9kYJlTubQRSk1%2B27PEVG43wt%2B9crl9LWSL98bL%2Fw23VV1AZPXYrCWybQrL%2FSHyBcA7z%2BHcqkxiT50rqDbt4XHsPTrX8fIMTsNwIy6qNRTXt%5CCiEWUX8e2KLwdf9ObNWrPMe%2FbcsA%2B9UBLNcpp5RvkHha%3A1638017332139; YD00517437729195%3AWM_NI=U65x3wh2yZ4EFPa8t3R%2BGfXOsA6eUZOMAqYgh38kbhuxodUCYnTbDzibBCxmgCpvle2ClLdStneymtGRMCB01JbMilzfbgAWnWHGTIJ1VgCJr6Fp9lxUeuyF6x9rOzR6ZjE%3D; YD00517437729195%3AWM_NIKE=9ca17ae2e6ffcda170e2e6ee97c44a86a69d83e867bc9a8ba2d55b868b8faef565f1b49883c540a7eca1bbf82af0fea7c3b92a97eafb99b27daceeb7bbb74f8690afd9f73992f58987db45b19ffa86d8528d938f92dc6dacedf88ce164b7a8a185d8499bb29c83c772abbe8ea4fb67a59dc097ef6ff29da4ade759b8b2ab9af080858e83a4c13cf396aeaeec68babaae88c45dae8988a8d7438aef9a88c242b2ea8a91bc68bc8bab96fc4ef6a8f793ce7292ea968bc837e2a3; captcha_session_v2="2|1:0|10:1638016733|18:captcha_session_v2|88:eWpsQUVzdlJaOWwvTENVVHFGNlNKWU1uT3J2c1ROR29EeUdZQlp0VjA4bk9Vcm5idW93Y1Z2RklGeWhaMUJMZg==|a7beb7de9655c9b7e73cf598813899ebb9fa839550316b12a87c27e15d763aa0"; captcha_ticket_v2="2|1:0|10:1638016741|17:captcha_ticket_v2|704:eyJ2YWxpZGF0ZSI6IkNOMzFfd2MxcVRaZVBROFMxVndqWmo3RmVZNTVYVW1RNzE4TzZQaHBhX3Vlc21vRTZhT2RhNDllYmxBX18yV2RDdWx5T0I2dHpEb1dqZEJPZk5aV3A5ZjdYeVI1RGo2bFpGMklfTTdaMUg0cURXejRCUFJpSXI1OTh5SllwLWd5U25uRkdDMjJKUGhaTjhHUlRxVkFuRkRWbmF3NHBMUEpINWp0TXcybUtBS2t1VllKY05KcFhZUmltbVdoOUpObGFUczdJaDlCNURCTVEuVXByNHdGelV0NTdQWUpGcC5FTFJXMGxnNU81V2VubG82VnJfeWZyaXc0dGNXN0lIZDJqakM0MmgwRFE3Z3h5MmN0Z2NzQmFLRkpRbUJvOFFxdy1CVEJYbGR1ZjFzejlib1ZzNEY4MmlueS1WU2NLYlM4a0JxYnVmN1dtamg5cV9vOVVDajQxZ2RkZlFHS3hGNVI2ZDFDdU1saV9CQ2RyenBobEpwbGF2Zk1oaU5EZGpyZFk4TUM5aW9reTdsVU5pdkguRFlwck5ub3VnME1sdWhGOUdrVVR1LVpHWHNYZzVrS3pMb3hab2FnbGlNZ2FuLTZ3ejBFVHdObENoekZyRGlOV3ZLYWt5b2h4NHNYZzhOWU1vanM3ZnVhV2o0bWFoZE1CSUpNZFMySE9XeFhIWnpYMyJ9|a4f9ed4d1c834a0c353121aad139a02b9658ed81fc205004bcb0552d76f12758"; z_c0="2|1:0|10:1638016760|4:z_c0|92:Mi4xVjFSSkF3QUFBQUFBY0o5d2pTdjBFaVlBQUFCZ0FsVk4tSFNQWWdCWnU5dG44bVFWLWpyY3E3Ykh5MkRWb19GMF9B|6a2bf59a0106c8fb12ce5915a003d23d8a91bcf496b347fc76d4e45b25a42776"; unlock_ticket="ADAAezpzSAomAAAAYAJVTQAuomEEDU_dm5yTbKURZ__JDlb4Gki0mg=="; NOT_UNREGISTER_WAITING=1; Hm_lpvt_98beee57fd2ef70ccdd5ca52b9740c49=1638016858; KLBRSID=0a401b23e8a71b70de2f4b37f5b4e379|1638016961|1638016429
+#     zs.zhihu_user_info_spider("https://www.zhihu.com/people/chen-shao-neo", cookie)
